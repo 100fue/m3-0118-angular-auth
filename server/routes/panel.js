3f9f935826a24f396ccc25488c01e44deeadf7b1
100fue <malvacien@gmail.com>
2018-03-07 11:55:40 +0100

    update user profile

--- server/routes/panel.js
+++ server/routes/panel.js
@@ -3,18 +3,43 @@ const router = express.Router();
 const User = require('../models/User');
 const Animal = require('../models/Animal');
 const Support = require('../models/Support');
+const bcrypt = require('bcrypt');
+const debug = require('debug')("server:panel");
 
 
 router.get('/panel/:id', (req, res, next) => {
     const userId = req.params.id
-    Support.find({userId: userId})
-    .populate('animalId')
+    Support.find({ userId: userId })
+        .populate('animalId')
         .then(support => {
             console.log("support")
             console.log(support)
             res.json(support).status(200);
         })
         .catch(err => res.json(err).status(500));
+});
+
+router.post('/panel/:id', (req, res, next) => {
+    const { username, password } = req.body;
+    const userId = req.params.id;
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashPass = bcrypt.hashSync(password, salt);
+    const theUser = new User({
+        username,
+        password: hashPass
+    });
+
+    User.findByIdAndUpdate({ userId: userId }, { theUser }, { new: true })
+        .then(updatedUser => {
+            debug(`Changes user ${user._id}. Change ${user.username}`);
+            req.user = updatedUser
+            res.status(200).json(req.user)
+        })
+        .catch(e => {
+            console.log(e);
+            res.status(500).json(e)
+        })
 });
 
 module.exports = router;
